bca96bd58244413baa731d5fd7c8b08fa74666a9
litonka <134679927+litonka@users.noreply.github.com>
2023-06-05 21:54:50 +0900

    u
    u

--- src/exercises01_49/Exercise01.java
+++ src/exercises01_49/Exercise01.java
@@ -4,8 +4,6 @@ public class Exercise01 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello,world!");
-		System.out.println("Hello,world!");
-		System.out.println("Hello,world!");
 
 	}
 
