72dbeb4a2d64865ddb405d99fa954a17281edde2
litonka <134679927+litonka@users.noreply.github.com>
2023-06-06 18:07:37 +0900

    u
    u

---
+++ src/chart01_03/Chart02Q4.java
@@ -0,0 +1,18 @@
+package chart01_03;
+
+/**
+ * 質問４︓以下式の値を記載してください。
+ * 1. 1.0 + 2.0
+ * 2. 1.0 + “2.0“
+ * @author liton
+ *
+ */
+public class Chart02Q4 {
+
+	public static void main(String[] args) {
+		System.out.println(1.0 + 2.0);//3.0
+		System.out.println(1.0 + "2.0");//1.02.2
+
+	}
+
+}
---
+++ src/chart01_03/Chart02Q5.java
@@ -0,0 +1,22 @@
+package chart01_03;
+
+/**
+ * 質問５︓以下ステートメントの出⼒を記載してください。
+ * 
+public static void main(String[] args) {
+ int a = 10;
+ System.out.println("a");
+}
+ * 
+ * @author liton
+ *
+ */
+public class Chart02Q5 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println("a");//a
+
+	}
+
+}
