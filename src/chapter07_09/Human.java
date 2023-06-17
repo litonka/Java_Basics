54bbb1b4d6ca283363b6bad693a692c54212a70f
litonka <134679927+litonka@users.noreply.github.com>
2023-06-17 23:48:22 +0900

    11
    11

--- src/chapter07_09/Human.java
+++ src/chapter07_09/Human.java
@@ -1,6 +1,44 @@
 package chapter07_09;
 
 public class Human {
+	/*	//属性：名称を定義する
+		public String name;
 	
-
+		//コンストラクタ
+		public Human() {
+			//コンストラクタに初期化処理を行う
+			//コンストラクタ詳細は後で説明します。
+			name = "名前なし";
+		}
+	
+		//属性名称に対する操作
+		public String getName() {
+			return name;//該当クラスの属性：名称を戻します。
+		}*/
+	
+	
+	
+	/*	private String name;
+		protected int age = 10;
+	
+		public String getName() {
+			return name;
+		}
+	*/
+	
+	public String name;
+	protected int age;
+	private int money;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name =name;
+	}
+	
+	public void sleep() {
+		
+	}
 }
