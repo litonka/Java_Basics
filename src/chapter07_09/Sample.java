991b1ba58341dc655c84608a9ced58a4b79fc5dc
litonka <134679927+litonka@users.noreply.github.com>
2023-06-17 23:39:56 +0900

    1
    1

---
+++ src/chapter07_09/Sample.java
@@ -0,0 +1,10 @@
+package chapter07_09;
+
+public class Sample {
+	public static void main(String... args) {
+		Human man = new Human();
+//		System.out.println(man.name); //NG, nameはprivateの為、アクセス不可
+		System.out.println(man.age);//OK, ageはprotected、同じpackageクラスアクセス可能
+		System.out.println(man.getName());//OK,　publicのため、いつでもどこでもアクセス可能
+	}
+}
