5814906b9bc5ab09197281b541915e2c2171b410
litonka <134679927+litonka@users.noreply.github.com>
2023-06-21 22:26:57 +0900

    u
    u

--- src/ch10/Method01.java
+++ src/ch10/Method01.java
@@ -3,7 +3,18 @@ package ch10;
 public class Method01 {
 	public static void main(String[] args) {
 		System.out.println("a45bc".charAt(1));
-		System.out.println("abc".compareTo("ab2c"));
+		System.out.println("abc".compareTo("abc"));
+		System.out.println("aBca".compareToIgnoreCase("abcd"));
+		System.out.println("abc".concat("123"));
+		System.out.println("abc".equals("123"));
+		System.out.println("ABs".equalsIgnoreCase("Abs"));
+
+		System.out.println("bba".indexOf("a"));
+		System.out.println("ccd".lastIndexOf("c"));
+		System.out.println("a245bc".length());
+
+		System.out.println("abadx234".replace("a", "g"));
+
 	}
 
 }
