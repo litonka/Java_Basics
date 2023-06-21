32b64d643a949167b1255b345f4f9522419bc016
litonka <134679927+litonka@users.noreply.github.com>
2023-06-21 22:42:43 +0900

    u
    u

---
+++ src/chapter07_09Test/BasePet.java
@@ -0,0 +1,10 @@
+package chapter07_09Test;
+
+public abstract class BasePet {
+	public abstract void hello();
+
+	public void eat() {
+
+	}
+
+}
--- src/chapter07_09Test/Test.java
+++ src/chapter07_09Test/Test.java
@@ -3,10 +3,7 @@ package chapter07_09Test;
 public class Test {
 
 	public static void main(String[] args) {
-		Object str = new String("DCNET");
-		String value1 = (String) str;
-		String value2 = String.class.cast(value1);
-		String excep = (int) str;
+		
 	}
 
 }
