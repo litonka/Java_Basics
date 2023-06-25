e0c0ad1f13a4b96a1ded1da1ed4332e4fffdbab0
litonka <134679927+litonka@users.noreply.github.com>
2023-06-25 23:59:27 +0900

    11
    11

---
+++ src/ch10/Q3.java
@@ -0,0 +1,16 @@
+package ch10;
+
+/**
+ * 質問３︓Javaには、すべての⽂字コードはUTF-8であり。⽇本語（全⾓⽂字）の場合１⽂
+字 = 3バイト。以下⽂字列のバイト数と桁数を求めてください。
+1public static void main(String...args) {
+2 String value = ”abced12345あいうえお１２３４５”;
+3 // バイト数は？
+4 // 桁数は？
+5}
+ * @author liton
+ *
+ */
+public class Q3 {
+
+}
