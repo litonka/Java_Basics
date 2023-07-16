b322892c95e15b1c18ab3f87b2cedd751ddac485
litonka <134679927+litonka@users.noreply.github.com>
2023-07-16 23:56:37 +0900

    u
    u

---
+++ src/ch09/Q19.java
@@ -0,0 +1,14 @@
+package ch09;
+
+/**
+ * 質問１９︓以下Animalクラスを継承して以下２クラスを作成してください。
+1. クラス Cat。sayHelloメソッドをオーバーライドして、System.out.println(“ニャー“);
+2. クラス Dog。sayHelloメソッドをオーバーライドして、System.out.println(“ワン“);
+※親クラスに明⽰的なコンストラクタが存在しない場合、サブクラスは必ず親クラスのコ
+ンストラクタをオーバーライドします。
+ * @author liton
+ *
+ */
+public class Q19 {
+
+}
--- src/ch09/Q20.java
+++ src/ch09/Q20.java
@@ -1,11 +1,10 @@
 package ch09;
+
 /**
  * 質問２０︓以下コードをIS-A関係で説明してください。
-1
-2
 System.out.println("1234"); // 静的のField
 String value = String.format("%05d", 0); // 静的のメソッド
-1 System.out.println(new Animal("cat").getName());
+System.out.println(new Animal("cat").getName());
 Animal v0 = new Animal(); // v0 は Animal 型
 Animal v1 = new Cat("a cat"); // v1 は Cat 型, v1 は Animal 型
 Animal v2 = new Dog("a dog"); // v2 は Dog 型, v2 は Animal 型
